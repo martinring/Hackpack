0d512cfda610f40e335193377260e67e335cc23f
Martin Ring <martin.ring@dfki.de>
2017-03-07 19:20:41 +0100

    wip

--- Util.hs
+++ Util.hs
@@ -1,5 +1,6 @@
 module Util where
 
+import Data.Maybe
 import Data.Bits (Bits,testBit)
 import Data.List (partition,sortOn)
 import Control.Arrow ((***))
@@ -16,3 +17,8 @@ partitionEvery n = (map snd *** map snd) . partition ((0==) . (`mod` n) . fst) .
 
 closest :: (Ord a, Num a) => a -> [a] -> [a]
 closest a = map fst . sortOn snd . map (\x -> (x,abs (a-x)))
+
+weightedIterate :: [[a]] -> [a]
+weightedIterate opts = concat now ++ weightedIterate later
+  where len = length opts
+        (now,later) = unzip $ zipWith (\i -> splitAt (len - i)) [0..] opts
