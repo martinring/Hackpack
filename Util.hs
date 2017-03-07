9e83c0c8760dc218eee824e8ffbb778a019d9ef9
Martin Ring <martin.ring@dfki.de>
2017-03-07 09:15:38 +0100

    initial commit

---
+++ Util.hs
@@ -0,0 +1,18 @@
+module Util where
+
+import Data.Bits (Bits,testBit)
+import Data.List (partition,sortOn)
+import Control.Arrow ((***))
+
+bools :: Bits b => b -> [Bool]
+bools b = map (testBit b) [0..]
+
+bitFilter :: Bits b => b -> [a] -> [a]
+bitFilter bits = map snd . filter fst . zip (bools bits)
+
+partitionEvery :: Int -> [a] -> ([a],[a])
+partitionEvery 0 = \x -> (x,[])
+partitionEvery n = (map snd *** map snd) . partition ((0==) . (`mod` n) . fst) . zip [0..]
+
+closest :: (Ord a, Num a) => a -> [a] -> [a]
+closest a = map fst . sortOn snd . map (\x -> (x,abs (a-x)))
